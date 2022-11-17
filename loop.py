979555f21b020490129aa6afcad95cc208b0697b
blaze-Youssef <83608661+blaze-Youssef@users.noreply.github.com>
2022-11-17 20:35:49 +0100

    improvements

--- loop.py
+++ loop.py
@@ -12,9 +12,9 @@ try:
             job1 = executor.submit(src.storing.refresh_exchanges_btc)
             job2 = executor.submit(src.storing.refresh_exchanges_eth)
             job3 = executor.submit(src.storing.refresh_exchanges_sol)
-            job1.result(timeout=500)
-            job2.result(timeout=500)
-            job3.result(timeout=500)
+            job1.result()
+            job2.result()
+            job3.result()
 
 
 except KeyboardInterrupt:
