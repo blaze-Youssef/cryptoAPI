196e53274088a76df02baaf7e2dddca97067ae8b
blaze-Youssef <83608661+blaze-Youssef@users.noreply.github.com>
2022-11-18 17:45:47 +0100

    Improvements

--- loop.py
+++ loop.py
@@ -1,19 +1,25 @@
 import concurrent.futures
+from time import sleep
 
 import src.storing
 
 print("Service started...")
-
+CHECKFORERRORS = 60 * 5
 try:
 
-    while True:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            jobs = []
-            jobs.append(executor.submit(src.storing.refresh_exchanges_btc))
-            jobs.append(executor.submit(src.storing.refresh_exchanges_eth))
-            jobs.append(executor.submit(src.storing.refresh_exchanges_sol))
-            for job in jobs:
-                job.result()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+
+        job1 = executor.submit(src.storing.refresh_exchanges_btc)
+        job2 = executor.submit(src.storing.refresh_exchanges_eth)
+        job3 = executor.submit(src.storing.refresh_exchanges_sol)
+        while True:
+            sleep(CHECKFORERRORS)
+            if job1.done():
+                job1 = executor.submit(src.storing.refresh_exchanges_btc)
+            if job2.done():
+                job2 = executor.submit(src.storing.refresh_exchanges_eth)
+            if job3.done():
+                job3 = executor.submit(src.storing.refresh_exchanges_sol)
 
 
 except KeyboardInterrupt:
