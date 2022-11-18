6f74ecc5571217acf94c0f1d9370980186a8163e
blaze-Youssef <83608661+blaze-Youssef@users.noreply.github.com>
2022-11-18 17:31:33 +0100

    Database cpu uasage major improvment

--- src/storing.py
+++ src/storing.py
@@ -64,21 +64,27 @@ def get_iso():
 
 def refresh_exchanges_btc():
     Session.begin()
+    data_db = {}
     try:
         while True:
             sleep(10)
             for freq_id, freq in get_all_frequencies():
                 freq = freq.upper()
                 # Get Last update time for all BTC
-                query = (
-                    Session.query(
-                        Assetbtc.symbol_id, func.max(Assetbtc.time_period_end)
+                if data_db:
+                    data_b = list(data_db.items())
+                else:
+                    query = (
+                        Session.query(
+                            Assetbtc.symbol_id, func.max(Assetbtc.time_period_end)
+                        )
+                        .where(Assetbtc.frequency == freq_id)
+                        .group_by(Assetbtc.symbol_id)
+                        .order_by(func.max(Assetbtc.time_period_end).desc())
                     )
-                    .where(Assetbtc.frequency == freq_id)
-                    .group_by(Assetbtc.symbol_id)
-                    .order_by(func.max(Assetbtc.time_period_end).desc())
-                )
-                data_b = query.all()
+                    data_b = query.all()
+                    for symb, enddate in data_b:
+                        data_db[symb] = enddate
                 objs = []
                 for i in (1, 2):
                     if i == 2:
@@ -91,6 +97,7 @@ def refresh_exchanges_btc():
                         data_btc = api_call(
                             f"/v1/ohlcv/{symbol_id}/history?period_id={freq}&time_start={enddatetime.replace(microsecond=0).isoformat()}&time_end={get_iso()}&limit={LIMIT}"
                         )
+                        is_data = False
                         for data_bt in data_btc:
                             obj = Assetbtc(
                                 symbol_id=symbol_id,
@@ -111,6 +118,12 @@ def refresh_exchanges_btc():
                                 frequency=freq_id,
                             )
                             objs.append(obj)
+                            if not is_data:
+                                is_data = True
+                        if is_data:
+                            data_db[symbol_id] = objs[-1].time_period_end.replace(
+                                tzinfo=None
+                            )
                 if objs:
                     Session.add_all(objs)
                     Session.commit()
@@ -126,21 +139,27 @@ def refresh_exchanges_btc():
 
 def refresh_exchanges_eth():
     Session.begin()
+    data_db = {}
     try:
         while True:
             sleep(10)
             for freq_id, freq in get_all_frequencies():
                 freq = freq.upper()
                 # Get Last update time for all ETH
-                query = (
-                    Session.query(
-                        Asseteth.symbol_id, func.max(Asseteth.time_period_end)
+                if data_db:
+                    data_b = list(data_db.items())
+                else:
+                    query = (
+                        Session.query(
+                            Asseteth.symbol_id, func.max(Asseteth.time_period_end)
+                        )
+                        .where(Asseteth.frequency == freq_id)
+                        .group_by(Asseteth.symbol_id)
+                        .order_by(func.max(Asseteth.time_period_end).desc())
                     )
-                    .where(Asseteth.frequency == freq_id)
-                    .group_by(Asseteth.symbol_id)
-                    .order_by(func.max(Asseteth.time_period_end).desc())
-                )
-                data_b = query.all()
+                    data_b = query.all()
+                    for symb, enddate in data_b:
+                        data_db[symb] = enddate
                 objs = []
                 for i in (1, 2):
                     if i == 2:
@@ -153,6 +172,7 @@ def refresh_exchanges_eth():
                         data_eth = api_call(
                             f"/v1/ohlcv/{symbol_id}/history?period_id={freq}&time_start={enddatetime.replace(microsecond=0).isoformat()}&time_end={get_iso()}&limit={LIMIT}"
                         )
+                        is_data = False
                         for data_bt in data_eth:
                             obj = Asseteth(
                                 symbol_id=symbol_id,
@@ -172,7 +192,15 @@ def refresh_exchanges_eth():
                                 trades_count=data_bt["trades_count"],
                                 frequency=freq_id,
                             )
+
                             objs.append(obj)
+                            if not is_data:
+                                is_data = True
+                        if is_data:
+                            data_db[symbol_id] = objs[-1].time_period_end.replace(
+                                tzinfo=None
+                            )
+
                 if objs:
                     Session.add_all(objs)
                     Session.commit()
@@ -188,6 +216,7 @@ def refresh_exchanges_eth():
 
 def refresh_exchanges_sol():
     Session.begin()
+    data_db = {}
     try:
         while True:
             sleep(10)
@@ -195,15 +224,20 @@ def refresh_exchanges_sol():
                 freq = freq.upper()
 
                 # Get Last update time for all SOL
-                query = (
-                    Session.query(
-                        Assetsol.symbol_id, func.max(Assetsol.time_period_end)
+                if data_db:
+                    data_b = list(data_db.items())
+                else:
+                    query = (
+                        Session.query(
+                            Assetsol.symbol_id, func.max(Assetsol.time_period_end)
+                        )
+                        .where(Assetsol.frequency == freq_id)
+                        .group_by(Assetsol.symbol_id)
+                        .order_by(func.max(Assetsol.time_period_end).desc())
                     )
-                    .where(Assetsol.frequency == freq_id)
-                    .group_by(Assetsol.symbol_id)
-                    .order_by(func.max(Assetsol.time_period_end).desc())
-                )
-                data_b = query.all()
+                    data_b = query.all()
+                    for symb, enddate in data_b:
+                        data_db[symb] = enddate
                 objs = []
                 for i in (1, 2):
                     if i == 2:
@@ -216,6 +250,7 @@ def refresh_exchanges_sol():
                         data_sol = api_call(
                             f"/v1/ohlcv/{symbol_id}/history?period_id={freq}&time_start={enddatetime.replace(microsecond=0).isoformat()}&time_end={get_iso()}&limit={LIMIT}"
                         )
+                        is_data = False
                         for data_bt in data_sol:
                             obj = Assetsol(
                                 symbol_id=symbol_id,
@@ -236,6 +271,12 @@ def refresh_exchanges_sol():
                                 frequency=freq_id,
                             )
                             objs.append(obj)
+                            if not is_data:
+                                is_data = True
+                        if is_data:
+                            data_db[symbol_id] = objs[-1].time_period_end.replace(
+                                tzinfo=None
+                            )
                 if objs:
                     Session.add_all(objs)
                     Session.commit()
