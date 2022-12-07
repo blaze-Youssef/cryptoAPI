afc09497b062162ab51010534a955ab5d86acccd
blaze-Youssef <83608661+blaze-Youssef@users.noreply.github.com>
2022-12-07 23:33:21 +0100

    improved logging

--- loop.py
+++ loop.py
@@ -2,8 +2,15 @@ import concurrent.futures
 from time import sleep
 
 import src.storing
+from src.methods import setup_logger
+
+logger = setup_logger(
+    "loop_logger",
+    "./log/loop.txt",
+)
 
 print("Service started...")
+logger.info("Service started.")
 CHECKFORERRORS = 60 * 5
 try:
 
@@ -12,15 +19,21 @@ try:
         job1 = executor.submit(src.storing.refresh_exchanges_btc)
         job2 = executor.submit(src.storing.refresh_exchanges_eth)
         job3 = executor.submit(src.storing.refresh_exchanges_sol)
+        logger.info("Jobs submitted successfuly.")
         while True:
             sleep(CHECKFORERRORS)
             if job1.done():
                 job1 = executor.submit(src.storing.refresh_exchanges_btc)
+                logger.error("BTC job ended, resubmitted successfuly.")
+
             if job2.done():
                 job2 = executor.submit(src.storing.refresh_exchanges_eth)
+                logger.error("ETH job ended, resubmitted successfuly.")
             if job3.done():
                 job3 = executor.submit(src.storing.refresh_exchanges_sol)
+                logger.error("SOL job ended, resubmitted successfuly.")
 
 
 except KeyboardInterrupt:
     print("Interrupt..")
+    logger.info("Interrupt..")
